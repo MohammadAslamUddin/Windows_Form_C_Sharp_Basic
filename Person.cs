2a26eb3230b144d356056a7205ff3cd3fba8634f
MohammadAsalmUddin <aslamuddin0015@gmail.com>
2021-03-04 14:49:26 +0600

    #User Defined Class

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormApp
+{
+    class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string fathersName { get; set; }
+        public string mothersName { get; set; }
+        public string address { get; set; }
+    }
+}
---
+++ UserDefinedPersonalInfoUI.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormApp
+{
+    public partial class UserDefinedPersonalInfoUI : Form
+    {
+        public UserDefinedPersonalInfoUI()
+        {
+            InitializeComponent();
+        }
+
+        Person person = new Person();
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            
+            person.firstName = firstNameTextBox.Text;
+            person.lastName = lastNameTextBox.Text;
+            person.fathersName = fathersNameTextBox.Text;
+            person.mothersName = mothersNameTextBox.Text;
+            person.address = addressTextBox.Text;
+
+            ClearAllText();
+        }
+
+        private void ClearAllText()
+        {
+            firstNameTextBox.Text = String.Empty;
+            lastNameTextBox.Text = String.Empty;
+            fathersNameTextBox.Text = String.Empty;
+            mothersNameTextBox.Text = String.Empty;
+            addressTextBox.Text = String.Empty;
+        }
+
+        private void nameButton_Click(object sender, EventArgs e)
+        {
+            ClearAllText();
+
+            firstNameTextBox.Text = person.firstName;
+            lastNameTextBox.Text = person.lastName;
+        }
+
+        private void fathersNameButton_Click(object sender, EventArgs e)
+        {
+            ClearAllText();
+
+            fathersNameTextBox.Text = person.fathersName;
+        }
+
+        private void mothersNameButton_Click(object sender, EventArgs e)
+        {
+            ClearAllText();
+
+            mothersNameTextBox.Text = person.mothersName;
+        }
+
+        private void addressButton_Click(object sender, EventArgs e)
+        {
+            ClearAllText();
+
+            addressTextBox.Text = person.address;
+        }
+
+        private void showAllInfoButton_Click(object sender, EventArgs e)
+        {
+            ClearAllText();
+
+            firstNameTextBox.Text = person.firstName;
+            lastNameTextBox.Text = person.lastName;
+            fathersNameTextBox.Text = person.fathersName;
+            mothersNameTextBox.Text = person.mothersName;
+            addressTextBox.Text = person.address;
+        }
+    }
+}
